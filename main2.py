4ced7eaed410f2c3093c993e6529e28bbf2d1ca6
BuildTools <unconfigured@null.spigotmc.org>
2024-05-20 10:03:43 +0300

    Working movement

--- main2.py
+++ main2.py
@@ -9,40 +9,63 @@ from pygame.draw import *
 
 pygame.init()
 
+
 def square(surf, col, point1, point2, point3, point4):
-    polygon(surf, col, ((point1, point2), (point2, point3), (point3, point4), (point4, point1)))
+    polygon(
+        surf,
+        col,
+        ((point1, point2), (point2, point3), (point3, point4), (point4, point1)),
+    )
+
 
 class wall:
-    def __init__(self, surf: pygame.Surface, width: float, height: float, depth: float, x: float, y: float, z: float, col: pygame.color, playerPosition: list[float|float|float]):
+    def __init__(
+        self,
+        surf: pygame.Surface,
+        width: float,
+        height: float,
+        depth: float,
+        x: float,
+        y: float,
+        z: float,
+        col: pygame.color,
+        playerPosition: list[float | float | float],
+    ):
         self.win: pygame.Surface = surf
-        self.dim: list[float|float|float] = [width, height, depth]
-        self.xyz: list[float|float|float] = self.convert3Dto2D([x, y, z])
+        self.dim: list[float | float | float] = [width, height, depth]
+        self.xyz: list[float | float | float] = self.convert3Dto2D([x, y, z])
         self.color: pygame.Color = col
-        self.playerPos: list[float|float|float] = playerPosition
-        self.rot: float = m.atan2(self.xyz[2] - self.playerPos[2], self.xyz[0] - self.playerPos[0])
+        self.playerPos: list[float | float | float] = playerPosition
+        self.rot: float = m.atan2(
+            self.xyz[2] - self.playerPos[2], self.xyz[0] - self.playerPos[0]
+        )
 
         self.centerX: int = int(get_window_size()[0] / 2)
         self.centerY: int = int(get_window_size()[1] / 2)
 
+
 Point = namedtuple("Point", "X Y")
 ColorTuple = namedtuple("Color", "R G B")
+
+
 def main():
     sizeX = 850
     sizeY = 650
     win = set_mode((sizeX, sizeY))
-    horizonX = sizeX / 2
-    horizonY = sizeY / 2 + 100
-    rot = 0
+    xHorizon_0 = sizeX / 2
+    yHorizon_0 = sizeY / 2 + 100
+    xHorizon = xHorizon_0
     defaultColor = ColorTuple(255, 255, 255)
-    walls = (
-        [(1, 1, 1), (1, 0, 1), defaultColor],
-    )
+    walls = ([(1, 1, 1), (1, 0, 1), defaultColor],)
 
     gridSize = 30
+    xGridSize = yGridSize = gridSize
     xOffset = 0
     yOffset = 5
+    velocity = 0.3
     while True:
         win.fill((0, 0, 0))
+        yHorizon = yHorizon_0 + (xHorizon - xHorizon_0) ** 2 / 1000
 
         for e in event.get():
             if e.type == QUIT:
@@ -55,27 +78,34 @@ def main():
             pygame.quit()
             sys.exit()
         if k[K_UP]:
-            yOffset = (yOffset +  0.1) % gridSize
+            yHorizon_0 -= velocity
         if k[K_DOWN]:
-            yOffset = (yOffset - 0.1) % gridSize
-        # if k[K_LEFT]:
+            yHorizon_0 += velocity
+        if k[K_LEFT]:
+            xHorizon = xHorizon + velocity
+        if k[K_RIGHT]:
+            xHorizon = xHorizon - velocity
         if k[K_d]:
-            xOffset = (xOffset -  0.1) % gridSize
+            xOffset = (xOffset - velocity) % xGridSize
         if k[K_a]:
-            xOffset = (xOffset + 0.1)   % gridSize
+            xOffset = (xOffset + velocity) % xGridSize
+        if k[K_w]:
+            yOffset = (yOffset + velocity) % yGridSize
+        if k[K_s]:
+            yOffset = (yOffset - velocity) % yGridSize
 
+        yGridSize = max(int(round(gridSize * (sizeY - yHorizon) / sizeY * 2)), 1)
 
+        line(win, defaultColor, (0, yHorizon), (sizeX, yHorizon))
 
-        line(win, defaultColor, (0, horizonY), (sizeX, horizonY))
-
-        xDrift = sizeX
-        for i in range(-xDrift, sizeX + xDrift+1, gridSize):
+        xDrift = 10 * sizeX
+        for i in range(-xDrift, sizeX + xDrift + 1, xGridSize):
             _xEndPosition = i + xOffset
             # print(_xEndPosition)
-            line(win, defaultColor, (horizonX, horizonY), (_xEndPosition, sizeY))
+            line(win, defaultColor, (xHorizon, yHorizon), (_xEndPosition, sizeY))
 
-        for i in range(int(horizonY) // gridSize, sizeY+1):
-            line(win, defaultColor, (0, i * gridSize + yOffset), (sizeX, i * gridSize + yOffset))
+        for i in range(int(yHorizon), sizeY + 1, yGridSize):
+            line(win, defaultColor, (0, i + yOffset), (sizeX, i + yOffset))
 
         flip()
 
